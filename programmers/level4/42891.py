1cfe9cbfcb8eefb9fcfd7124fc7a0271e45e235b
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2020-11-09 23:49:58 +0900

    Feat : 무지의 먹방 라이브 풀이완료

--- programmers/level4/42891.py
+++ programmers/level4/42891.py
@@ -1,3 +1,18 @@
-from sys import stdin
-READ = lambda : stdin.readline().strip()
+### 무지의 먹방 라이브
+import heapq
+def solution(food_times, k):
+    food = [(time, idx) for idx, time in enumerate(food_times, 1)]
+    heapq.heapify(food)
 
+    smallest = food[0][0]
+    prev = 0
+    while k - ((smallest - prev) * len(food)) >= 0:
+        k -= (smallest - prev) * len(food)
+        prev, idx = heapq.heappop(food)
+
+        if not food:
+            return -1
+
+        smallest = food[0][0]
+    food = sorted(food, key=lambda x:x[1])
+    return food[k % len(food)][1]
