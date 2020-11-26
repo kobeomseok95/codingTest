aee76988bd047445c4c12bd1a36e45865d9056f1
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2020-11-26 23:55:11 +0900

    Feat : 백준 2887 풀이

---
+++ boj/gold/2887.py
@@ -0,0 +1,48 @@
+from sys import stdin
+READ = lambda : stdin.readline().strip()
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, x, y):
+    x = find_parent(parent, x)
+    y = find_parent(parent, y)
+    if x > y:
+        parent[x] = y
+    else:
+        parent[y] = x
+
+
+n = int(READ())
+parent = [i for i in range(n + 1)]
+
+x, y, z = [], [], []
+for i in range(1, n + 1):
+    a, b, c = map(int, READ().split())
+    x.append((a, i))
+    y.append((b, i))
+    z.append((c, i))
+
+x.sort()
+y.sort()
+z.sort()
+
+edges = []
+for i in range(n - 1):
+    edges.append((x[i+1][0] - x[i][0], x[i][1], x[i+1][1]))
+    edges.append((y[i+1][0] - y[i][0], y[i][1], y[i+1][1]))
+    edges.append((z[i+1][0] - z[i][0], z[i][1], z[i+1][1]))
+
+edges.sort()
+result = 0
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
