f9c7ed6171f24f36aa0a7a07eda4278d243c3498
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2020-12-05 15:53:12 +0900

    Update : 백준 연구소 수정코드

--- boj/gold/14502_2.py
+++ boj/gold/14502_2.py
@@ -1,19 +1,19 @@
 ### 다시 풀어보기
 from sys import stdin
+from itertools import combinations
 read = lambda : stdin.readline().strip()
-
-n, m = map(int, read().split())
-maps = []
-for _ in range(n):
-    maps.append(list(map(int, read().split())))
-
-tmp = [[0 for _ in range(m)] for _ in range(n)]
-
-dy, dx = [-1, 0, 1, 0], [0, -1, 0, 1]
-result = 0
+dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 
-def get_score():
+def spread_virus(y, x):
+    for i in range(4):
+        ny, nx = y + dy[i], x + dx[i]
+        if 0 <= ny < n and 0 <= nx < m:
+            if tmp[ny][nx] == 0:
+                tmp[ny][nx] = 2
+                spread_virus(ny, nx)
+
+def check_save_zone():
     count = 0
     for i in range(n):
         for j in range(m):
@@ -22,40 +22,57 @@ def get_score():
     return count
 
 
-def virus(y, x):
-    for i in range(4):
-        ny, nx = y + dy[i], x + dx[i]
-        if 0 <= ny < n and 0 <= nx < m:
-            if tmp[ny][nx] == 0:
-                tmp[ny][nx] = 2
-                virus(ny, nx)
+n, m = map(int, read().split())
+maps, safety = [], []
+answer = 0
+tmp = [[0 for _ in range(m)] for _ in range(n)]
 
+for i in range(n):
+    maps.append(list(map(int, read().split())))
+    for j in range(m):
+        if maps[i][j] == 0:
+            safety.append((i, j))
 
-def dfs(count):
-    global result
-
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                tmp[i][j] = maps[i][j]
-
-        for i in range(n):
-            for j in range(m):
-                if tmp[i][j] == 2:
-                    virus(i, j)
-
-        result = max(result, get_score())
-        return
-
+# 안전지대중 3곳을 조합으로 뽑는다.
+for save_zone in combinations(safety, 3):
+    # tmp에 일단 다 넣어주고
     for i in range(n):
         for j in range(m):
-            if maps[i][j] == 0:
-                count += 1
-                maps[i][j] = 1
-                dfs(count)
-                count -= 1
-                maps[i][j] = 0
+            tmp[i][j] = maps[i][j]
+
+    # save_zone을 벽으로 설정하기
+    for y, x in save_zone:
+        tmp[y][x] = 1
+
+    # 바이러스 퍼트리기
+    for i in range(n):
+        for j in range(m):
+            if tmp[i][j] == 2:
+                spread_virus(i, j)
+
+    safe = check_save_zone()
+    answer = max(answer, safe)
+
+print(answer)
 
 
-dfs(0)
-print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
