3f10d6da3993d650bda99c47ceb23a8f49d683b1
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2020-12-08 20:40:36 +0900

    Feat : 이진 탐색 2회독 완료

--- dongbinbook/chapter15/27_2.py
+++ dongbinbook/chapter15/27_2.py
@@ -1,19 +1,66 @@
-from bisect import bisect_left, bisect_right
 from sys import stdin
-READ = lambda : stdin.readline().strip()
+from bisect import bisect_left, bisect_right
+read = lambda : stdin.readline().strip()
 
 
-def count_by_range(arr, left, right):
-    right_idx = bisect_right(arr, right)
-    left_idx = bisect_left(arr, left)
+def find_idx(arr, left_value, right_value):
+    left_idx = bisect_left(arr, left_value)
+    right_idx = bisect_right(arr, right_value)
     return right_idx - left_idx
 
-n, x = map(int, READ().split())
-arr = list(map(int, READ().split()))
+n, x = map(int, read().split())
+arr = list(map(int, read().split()))
 
-count = count_by_range(arr, x, x)
+count = find_idx(arr, x, x)
+print("-1" if count == 0 else count)
 
-if count == 0:
-    print(-1)
-else:
-    print(count)
+# def find_first(arr, find_number, start, end):
+#     while start <= end:
+#         mid = (start + end) // 2
+#         if arr[mid] > find_number:
+#             end = mid - 1
+#         elif arr[mid] < find_number:
+#             start = mid + 1
+#         elif arr[mid] == find_number:
+#             for i in range(mid - 1, -1, -1):
+#                 if arr[i] != find_number:
+#                     return i + 1
+#
+#     return None
+#
+#
+# def find_last(arr, find_number, start, end):
+#     while start <= end:
+#         mid = (start + end) // 2
+#         if arr[mid] > find_number:
+#             end = mid - 1
+#         elif arr[mid] < find_number:
+#             start = mid + 1
+#         elif arr[mid] == find_number:
+#             for i in range(mid + 1, len(arr)):
+#                 if arr[i] != find_number:
+#                     return i - 1
+#
+#     return None
+#
+#
+# def count_by_value(arr, target):
+#     length = len(arr)
+#     a = find_first(arr, target, 0, length - 1)
+#
+#     if a == None:
+#         return 0
+#
+#     b = find_last(arr, target, 0, length - 1)
+#
+#     return b - a + 1
+#
+#
+# n, x = map(int, read().split())
+# arr = list(map(int, read().split()))
+#
+# count = count_by_value(arr, x)
+# if count == 0:
+#     print("-1")
+# else:
+#     print(count)
