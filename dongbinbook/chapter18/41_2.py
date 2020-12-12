83a24ce1920fa88c771fa503b5b5ea93b9cc9483
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2020-12-12 19:29:05 +0900

    Feat : 동빈북 그래프이론 여행계획 2회독 완료

--- dongbinbook/chapter18/41_2.py
+++ dongbinbook/chapter18/41_2.py
@@ -1,3 +1,43 @@
 from sys import stdin
-read = lambda : stdin.readline().split()
+read = lambda : stdin.readline().strip()
 
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, read().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, read().split())))
+
+plan = list(map(int, read().split()))
+
+parent = [0 for _ in range(n + 1)]
+for i in range(n + 1):
+    parent[i] = i
+
+for i in range(n):
+    for j in range(n):
+        if i != j and graph[i][j] == graph[j][i]:
+            union_parent(parent, i + 1, j + 1)
+
+possible = True
+for i in range(1, n + 1):
+    if parent[i] != 1:
+        possible = False
+        break
+
+print("YES" if possible else "NO")
