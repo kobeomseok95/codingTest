31a52edd0f2d89f5c29150b42f12108e03592de5
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-01-10 17:21:32 +0900

    Feat : 실패율 풀이 완료

--- programmers/level1/42889_4.py
+++ programmers/level1/42889_4.py
@@ -1,3 +1,17 @@
-def solution(N, stages):
-    answer = []
-    return answer
+def solution(n, stages):
+    people = len(stages)
+    non_clear = [0 for _ in range(n + 1)]
+    for i in range(people):
+        if stages[i] <= n:
+            non_clear[stages[i]] += 1
+
+    result = []
+    for i in range(1, n + 1):
+        if people <= 0:
+            result.append([i, 0])
+        else:
+            result.append([i, non_clear[i] / people])
+        people -= non_clear[i]
+
+    result = sorted(result, key=lambda x: (-x[1], x[0]))
+    return [i[0] for i in result]
