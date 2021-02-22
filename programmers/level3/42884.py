59a0e87fbf215c62207d2290415c5484a6de684f
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-02-22 18:49:49 +0900

    Feat : 프로그래머스 단속카메라 풀이

--- programmers/level3/42884.py
+++ programmers/level3/42884.py
@@ -1,27 +1,11 @@
 def solution(routes):
-    answer = 0
     routes = sorted(routes, key=lambda x: x[1])
-    check = []
+    answer = 0
+    camera = -30001
 
-    for i in range(len(routes)):
-        exit = routes[i][1]
-        if exit in check:
-            continue
-        for j in range(i, len(routes)):
-            if routes[j][0] <= exit <= routes[j][1]:
-                check.append(routes[j][1])
-        answer += 1
-
-        if len(check) == len(routes):
-            break
+    for start, end in routes:
+        if camera < start:
+            answer += 1
+            camera = end
 
     return answer
-
-
-def main():
-    a = solution([[-20,15], [-14,-5], [-18,-13], [-5,-3]])
-    print(a, a == 2)
-
-
-if __name__ == "__main__":
-    main()
